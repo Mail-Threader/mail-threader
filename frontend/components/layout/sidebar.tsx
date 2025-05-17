c3348d57f2b377a422ae90d3f8ca851427cae3a4
Siddhant Dalvi <siddhantdalvi3@gmail.com>
2025-05-17 19:05:05 +0200

    changes to sidebar

--- frontend/components/layout/sidebar.tsx
+++ frontend/components/layout/sidebar.tsx
@@ -1,13 +1,23 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 const Sidebar = ({
 	children,
 }: Readonly<{
 	children: ReactNode;
 }>) => {
-	return <div className="">{children}</div>;
+	useEffect(() => {}, []);
+
+	return (
+		<div id="sidebarContainer" className="w-full flex bg-blue-50">
+			<div
+				id="sidebar"
+				className="min-w-[5vw] max-w-[20vw] resize-x overflow-auto bg-yellow-100"
+			></div>
+			{children}
+		</div>
+	);
 };
 
 export default Sidebar;
